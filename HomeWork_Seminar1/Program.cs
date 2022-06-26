b6ffde462d00c855564ce87cddeb48b86e9cc485
artem <artem.titov.wg@gmail.com>
2022-06-26 15:08:28 +0300

    визуальные правки кода для простоты восприятия

--- HomeWork_Seminar1/Program.cs
+++ HomeWork_Seminar1/Program.cs
@@ -27,19 +27,19 @@ Console.WriteLine(min);
 // Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 
 Console.Write("Введите первое число: ");
-int num1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите второе число: ");
-int num2 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите третье число: ");
 int num3 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num4 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int num5 = Convert.ToInt32(Console.ReadLine());
 
-int max = num1;
+int max1 = num3;
 
-if (num2 > max) max = num2;
-if (num3 > max) max = num3;
+if (num4 > max1) max1 = num4;
+if (num5 > max1) max1 = num5;
 
 Console.Write("max = ");
-Console.WriteLine(max);
+Console.WriteLine(max1);
 
 // Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
 
