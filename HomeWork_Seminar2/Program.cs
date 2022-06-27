0cb10ff438494ab69bccfaccb2ac54599457d8fe
artem <artem.titov.wg@gmail.com>
2022-06-27 20:59:00 +0300

    Исправлена задача 2 по рекомендации преподавателя - сделать более обширный метод

--- HomeWork_Seminar2/Program.cs
+++ HomeWork_Seminar2/Program.cs
@@ -1,5 +1,6 @@
 ﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
-
+Console.WriteLine("Задача 1");
+Console.WriteLine();
 int SecondNum(int num)
 {
     int del = num / 10;
@@ -20,30 +21,32 @@ if (num < 100)
 }
 int result = SecondNum(num);
 Console.WriteLine($"Вторая цифра введённого числа равна {result}");
-
+Console.WriteLine();
 // Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
-
+Console.WriteLine("Задача 2");
+Console.WriteLine();
 int ThirdNum(int num1)
 {
+    while (num1 > 999)
+    {
+        num1 = num1 / 10;
+    }
     int res = num1 % 10;
     return res;
 }
-int num1 = new Random().Next(10, 100000);
+int num1 = new Random().Next(10, 1000000000);
 Console.WriteLine(num1);
 if (num1 < 100)
 {
     Console.WriteLine($"У числа {num1} нет третьей цифры");
     return;
 }
-while (num1 > 999)
-{
-    num1 = num1 / 10;
-}
 int result1 = ThirdNum(num1);
 Console.WriteLine($"Третья цифра равна {result1}");
-
+Console.WriteLine();
 // Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
-
+Console.WriteLine("Задача 3");
+Console.WriteLine();
 bool Weekday(int num2)
 {
     if (num2 == 6) return true;
