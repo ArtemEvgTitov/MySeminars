bbaab8a62453a82c8a2e08a6072e4e7bc52f499d
artem <artem.titov.wg@gmail.com>
2022-07-03 16:56:43 +0300

    Доработана вторая задача

--- HomeWork_Seminar3/Program.cs
+++ HomeWork_Seminar3/Program.cs
@@ -34,25 +34,40 @@ double GetDistance(double x1, double y1, double z1, double x2, double y2, double
     double yLine = y2 - y1;
     double zLine = z2 - z1;
 
-    double distQuad = xLine*xLine + yLine*yLine + zLine*zLine;
+    double distQuad = xLine * xLine + yLine * yLine + zLine * zLine;
     double result = Math.Sqrt(distQuad);
 
     return result;
 }
 
+double z1 = 0;
+double z2 = 0;
+
+Console.Write("Введите коордитату x для первой точки ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите коордитату y для первой точки ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите коордитату x для второй точки ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите коордитату y для второй точки ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+
+double result1 = GetDistance(x1, y1, z1, x2, y2, z2);
+Console.WriteLine("Расстояние между точками равно " + result1);
+
 // Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 
 void Cube(int n)
 {
-	int count = 1;
-	while (count <= n)
-	{
-		int squad = count*count*count;
-		Console.Write(squad + " ");
-		count++;
-	}
+    int count = 1;
+    while (count <= n)
+    {
+        int squad = count * count * count;
+        Console.Write(squad + " ");
+        count++;
+    }
 }
 
 Console.Write("Введите число ");
-int n = Convert.ToInt32(Console.ReadLine());
-Cube(n);
+int n1 = Convert.ToInt32(Console.ReadLine());
+Cube(n1);
