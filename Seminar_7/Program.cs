926c1c6a4d3ea4c698eae65ca0d775c79b91bb0e
artem <artem.titov.wg@gmail.com>
2022-07-17 15:17:18 +0300

    упрощено решение последней задачи

--- Seminar_7/Program.cs
+++ Seminar_7/Program.cs
@@ -125,9 +125,8 @@ int SumArray(int[,] array)
 {
     int sum = 0;
     for (int i = 0; i < array.GetLength(0); i++)
-        for (int j = 0; j < array.GetLength(1); j++)
-            if (i == j)
-                sum += array[i, j];
+        sum += array[i, i];
+
     return sum;
 }
 
